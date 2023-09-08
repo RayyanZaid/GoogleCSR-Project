1e42060de2c8f8eefe54e527c30ead309738cb6f
RayyanZaid <rayyanzaid0401@gmail.com>
2023-09-08 12:18:37 -0700

    s

--- Data_Preprocessing/PossessionTracker/possessionCounter.py
+++ Data_Preprocessing/PossessionTracker/possessionCounter.py
@@ -4,7 +4,6 @@ import math
 # Define the path to your JSON file
 json_file_path = r"C:\Users\rayya\OneDrive\Desktop\GoogleCSR-Project\Data_Preprocessing\0021500524.json"
 
-
 # Open and read the JSON file
 with open(json_file_path, "r") as json_file:
     data = json.load(json_file)
@@ -13,8 +12,12 @@ with open(json_file_path, "r") as json_file:
 current_second = None
 current_quarter = None
 current_possessing_team = None
+prev_possessing_team = None
 current_shot_clock = None
 current_time_since_1970 = None
+
+num_possessions = 0
+possessing_iterations = 0  # Counter for consecutive iterations with the same possessing team
 
 # Iterate through each event
 for event in data["events"]:
@@ -29,11 +32,9 @@ for event in data["events"]:
         quarter = moment[0]
         current_shot_clock = moment[3]
 
-        if current_shot_clock == None:
+        if current_shot_clock is None:
             continue
-        # Round down the current second to the nearest whole second
-        current_second_rounded = math.floor(seconds_left)
-
+      
         # Extract the list of player positions from the moment
         player_positions = moment[5]
 
@@ -51,18 +52,35 @@ for event in data["events"]:
                     if distance < min_distance:
                         min_distance = distance
                         closest_player = player_id
+                        
                         current_possessing_team = team_id
 
             iteration_counter += 1
 
         # Check if the possession has changed
-        if current_possessing_team is not None and current_second_rounded != current_second:
-            current_second = current_second_rounded
+        if current_possessing_team != prev_possessing_team:
             current_quarter = quarter
-
+            prev_possessing_team = current_possessing_team
+            num_possessions += 1
+            possessing_iterations = 1  # Reset the possessing iterations count
             # Print which team has possession, shot clock, and quarter
-            print(f"Quarter: {current_quarter}, Possessing Team: {current_possessing_team}, Shot Clock: {current_shot_clock} seconds, Time: {current_time_since_1970}")
+            print(f"Quarter: {current_quarter}, Possessing Team: {current_possessing_team}, Shot Clock: {current_shot_clock} seconds, Seconds Left of Quarter: {seconds_left}")
+
+        else:
+            possessing_iterations += 1
+            if possessing_iterations > 5:
+                # If the same team has possession for more than 5 iterations, print it
+
+                clock_test = 'Quarter {:d} Time {:02d}:{:02d} Shot Clock {:03.1f}'.format(
+                         current_quarter,
+                         int(seconds_left) % 3600 // 60,
+                         int(seconds_left) % 60,
+                         current_shot_clock)
+                
+                print(f"{clock_test}, Possessing Team: {current_possessing_team}")
 
         # Stop checking possession after the first 5 iterations
         if iteration_counter >= 5:
             break
+
+print(f"Total Number of Possessions: {num_possessions}")
