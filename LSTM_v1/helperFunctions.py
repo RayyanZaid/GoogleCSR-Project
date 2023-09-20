c9f1b8b3c1917712d5c590e81c0e38160991a603
RayyanZaid <rayyanzaid0401@gmail.com>
2023-09-19 17:15:52 -0700

    inserted some dummy LSTM code. does not work yet data processing done

---
+++ LSTM_v1/helperFunctions.py
@@ -0,0 +1,111 @@
+from typing import List
+import numpy as np
+
+from MomentPreprocessing.MomentPreprocessingMain import MomentPreprocessingClass
+from Possession import Possession
+from getPossessionsFromCSV import getData
+from Moment import Moment
+from TemporalWindow import TemporalWindow
+
+
+T = 128 
+
+def createTemporalWindows(possessions: List[Possession]):
+    for eachPossession in possessions:
+        terminalActionIndex = eachPossession.terminalActionIndex
+        temporalWindows = []
+
+        if terminalActionIndex == -1:
+            if len(eachPossession.moments) >= T:
+                temporalWindow = TemporalWindow()
+                temporalWindow.moments = eachPossession.moments[-T:]
+                temporalWindow.label = 0
+
+
+                temporalWindows.append(temporalWindow)
+            else:
+                temporalWindow = TemporalWindow()
+                temporalWindow.moments = eachPossession.moments
+                temporalWindow.label = 0
+                temporalWindows.append(temporalWindow)
+        else:
+            numTemporalWindows = int((terminalActionIndex + 1) / T)
+            remainder = (terminalActionIndex + 1) % T
+            currentIndex = terminalActionIndex
+
+            for i in range(numTemporalWindows):
+                temporalWindowStartIndex = currentIndex - T
+                temporalWindowEndIndex = currentIndex
+                temporalWindow = TemporalWindow()
+                temporalWindow.moments = eachPossession.moments[temporalWindowStartIndex + 1:temporalWindowEndIndex + 1]
+                temporalWindow.label = eachPossession.moments[temporalWindowEndIndex].momentLabel
+                currentIndex = currentIndex - T
+                temporalWindows.insert(0, temporalWindow)
+
+            temporalWindow = TemporalWindow()
+            temporalWindow.moments = eachPossession.moments[temporalWindowStartIndex: temporalWindowStartIndex+remainder]
+            temporalWindow.label = eachPossession.moments[temporalWindowStartIndex].momentLabel
+            temporalWindows.insert(0, temporalWindow)
+
+        eachPossession.temporalWindows = temporalWindows
+        print("Added Temporal Windows of Possession with Terminal Action")
+
+
+def processDataForLSTM(possessions : List[Possession]):
+
+    inputTemporalWindowMatrix = []
+    outputTemporalWindowLabels = []
+
+    for eachPossession in possessions:
+
+        for eachTemporalWindow in eachPossession.temporalWindows:
+
+            momentObjectsOfTemporalWindow : List[Moment] = eachTemporalWindow.moments
+
+            momentDataOfTemporalWindow = []
+            labelDataOfTemporalWindow = [eachTemporalWindow.label]
+
+            for eachMoment in momentObjectsOfTemporalWindow:
+                momentDataOfTemporalWindow.append(eachMoment.momentArray)
+
+            # if(len(momentDataOfTemporalWindow) == 128):
+            
+            inputTemporalWindowMatrix.append(momentDataOfTemporalWindow)
+            outputTemporalWindowLabels.append(labelDataOfTemporalWindow)
+
+            
+
+
+    return inputTemporalWindowMatrix, outputTemporalWindowLabels
+
+
+
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Masking
+
+def loadAndTrainLSTM(inputData, labelData):
+
+    model = Sequential()
+
+    # Add a Masking layer to handle the masking automatically
+    model.add(Masking(mask_value=-1, input_shape=(128, 25)))  # Replace -1 with the appropriate mask value
+
+    # Add the LSTM layer with mask support
+    model.add(LSTM(units=64, activation='tanh', return_sequences=False, input_shape=(128, 25), mask_zero=True))  
+    # Set mask_zero=True to handle masking
+
+    # Add the output layer with sigmoid activation for binary classification
+    model.add(Dense(units=1, activation='sigmoid'))
+
+    # Compile the model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(inputData, labelData, epochs=10, batch_size=32)  
+    model.save("lstm_v1.h5")
+
+
+from keras.models import load_model
+
+def predict(testData):
+    loaded_model = load_model("lstm_v1.h5")
+    predictions = loaded_model.predict(testData)
+    print()
--- LSTM_v1/main.py
+++ LSTM_v1/main.py
@@ -20,16 +20,17 @@
     # Each element is the output label corresponding to the input temporal window
 
 
-# 5) Input each temporal window into the LSTM
+# 5) Input each temporal window into the LSTM and Train
 
 from typing import List
 import numpy as np
-
-from MomentPreprocessing.MomentPreprocessingMain import MomentPreprocessingClass
 from Possession import Possession
 from getPossessionsFromCSV import getData
 from Moment import Moment
-from TemporalWindow import TemporalWindow
+import helperFunctions 
+
+
+
 # 1) Read JSON and preprocess the data into a CSV
 
 # Already preprocessed into CSV
@@ -46,48 +47,8 @@ possessions : List[Possession] = getData()
 # 3) Go through possessions and create Temporal Windows
 
 
-T = 128 
-
-def createTemporalWindows(possessions: List[Possession]):
-    for eachPossession in possessions:
-        terminalActionIndex = eachPossession.terminalActionIndex
-        temporalWindows = []
-
-        if terminalActionIndex == -1:
-            if len(eachPossession.moments) >= T:
-                temporalWindow = TemporalWindow()
-                temporalWindow.moments = eachPossession.moments[-T:]
-                temporalWindow.label = 0
-                temporalWindows.append(temporalWindow)
-            else:
-                temporalWindow = TemporalWindow()
-                temporalWindow.moments = eachPossession.moments
-                temporalWindow.label = 0
-                temporalWindows.append(temporalWindow)
-        else:
-            numTemporalWindows = int((terminalActionIndex + 1) / T)
-            remainder = (terminalActionIndex + 1) % T
-            currentIndex = terminalActionIndex
-
-            for i in range(numTemporalWindows):
-                temporalWindowStartIndex = currentIndex - T
-                temporalWindowEndIndex = currentIndex
-                temporalWindow = TemporalWindow()
-                temporalWindow.moments = eachPossession.moments[temporalWindowStartIndex + 1:temporalWindowEndIndex + 1]
-                temporalWindow.label = eachPossession.moments[temporalWindowEndIndex].momentLabel
-                currentIndex = currentIndex - T
-                temporalWindows.insert(0, temporalWindow)
-
-            temporalWindow = TemporalWindow()
-            temporalWindow.moments = eachPossession.moments[temporalWindowStartIndex: temporalWindowStartIndex+remainder]
-            temporalWindow.label = eachPossession.moments[temporalWindowStartIndex].momentLabel
-            temporalWindows.insert(0, temporalWindow)
-
-        eachPossession.temporalWindows = temporalWindows
-        print("Added Temporal Windows of Possession with Terminal Action")
-
 # Now call the function
-createTemporalWindows(possessions)
+helperFunctions.createTemporalWindows(possessions)
 
 
 # 4) Process data so that it's in a input matrix : output vector format
@@ -97,36 +58,17 @@ createTemporalWindows(possessions)
     # Input : List of Possessions with Temporal Windows updated
     # Ouput : the input matrix and output vector
 
-def processDataForLSTM(possessions : List[Possession]):
+inputMatrix, outputVector = helperFunctions.processDataForLSTM(possessions)
 
-    inputTemporalWindowMatrix = []
-    outputTemporalWindowLabels = []
-
-    for eachPossession in possessions:
-
-        for eachTemporalWindow in eachPossession.temporalWindows:
-
-            momentObjectsOfTemporalWindow : List[Moment] = eachTemporalWindow.moments
-
-            momentDataOfTemporalWindow = []
-            labelDataOfTemporalWindow = [eachTemporalWindow.label]
-
-            for eachMoment in momentObjectsOfTemporalWindow:
-                momentDataOfTemporalWindow.append(eachMoment.momentArray)
-
-            
-            inputTemporalWindowMatrix.append(momentDataOfTemporalWindow)
-            outputTemporalWindowLabels.append(labelDataOfTemporalWindow)
-
-            
-
-
-    return inputTemporalWindowMatrix, outputTemporalWindowLabels
-
-inputMatrix, outputVector = processDataForLSTM(possessions)
-
-# inputMatrix = np.array(inputMatrix)
-outputVector = np.array(outputVector)
+# inputMatrix = np.array(inputMatrix)   # SHAPE: number of windows, 128, 25
+# outputVector = np.array(outputVector) # SHAPE: 5,1 
 
 # print(inputMatrix.shape)
-print(outputVector.shape)
+# print(outputVector.shape)
+
+
+
+# 5) Input each temporal window into the LSTM
+
+helperFunctions.loadAndTrainLSTM(inputMatrix,outputVector)
+helperFunctions.predict(inputMatrix)
