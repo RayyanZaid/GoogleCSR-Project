ba959b10eebcc597f28ae144e5f970983ec43cb3
RayyanZaid <rayyanzaid0401@gmail.com>
2023-09-30 14:32:17 -0700

    restructured

--- game_main.py
+++ game_main.py
@@ -16,7 +16,7 @@ import json
 
 
 if sys.gettrace() is not None:  # Check if debugger is active
-    args = argparse.Namespace(path=r"D:\coding\GoogleCSR-Project\LiveResults\0021500497.json" , event = 10)  # Provide default values for debugging
+    args = argparse.Namespace(path=r"D:\coding\GoogleCSR-Project\0021500497.json" , event = 10)  # Provide default values for debugging
 else:
     parser = argparse.ArgumentParser(description='Process arguments about an NBA game.')
     parser.add_argument('--path', type=str,
