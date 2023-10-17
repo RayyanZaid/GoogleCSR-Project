911cf4de942625e1c27ae99f34d3a65443f85c92
RayyanZaid <rayyanzaid0401@gmail.com>
2023-10-17 11:33:11 -0700

    processed 371 to 410

--- training_data_processing_script.py
+++ training_data_processing_script.py
@@ -121,7 +121,7 @@ def getInputOutputData(datasetDirectoryVariable):
 if __name__ == "__main__":
 
     # Specify the range of games to train
-    startGameNumber = 181
+    startGameNumber = 371
     endGameNumber = 600
     grouping_size = 5  # Number of games to process in each group
 
