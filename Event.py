81ea49eff40dc5f38d53e7d5d0f3967b9fc50619
RayyanZaid <rayyanzaid0401@gmail.com>
2023-10-22 22:26:50 -0700

    updated networks

--- Event.py
+++ Event.py
@@ -11,7 +11,7 @@ from typing import List
 from globals import WINDOW_SIZE, MOMENT_SIZE
 
 from keras.models import load_model
-model1 = load_model(r"stacked_LSTM")
+model1 = load_model(r"1D_Conv_LSTM")
 
 mapping = {
     0.0: "null",
